8d855898a20d68f1f2eb94edb0e6cd7e9b5c8063
Tom LanFranca <ittechtommy@gmail.com>
2018-02-27 11:47:01 -0600

    ignoring node modules

---
+++ scripts/script.js
@@ -0,0 +1,30 @@
+//FUNCTIONALITY FOR INFO BOX DISAPPERING ON SCROLL
+
+let header = document.querySelector(".header__info");
+let scrollDown = 0;
+
+window.addEventListener("scroll", () => {
+  let currentScroll = window.pageYOffset;
+
+  if (currentScroll > scrollDown) {
+    header.classList.add("header__info--hidden");
+  } else {
+    header.classList.remove("header__info--hidden");
+  }
+  
+  scrollDown = window.pageYOffset;
+});
+
+//FUNCTIONALITY FOR STICKY NAVIGATION BAR
+
+
+
+window.addEventListener("scroll", () => {
+  let navBar = document.querySelector(".header__nav");
+
+  if(window.pageYOffset >= 650) {
+    navBar.classList.add("header__nav--sticky");
+  } else {
+    navBar.classList.remove("header__nav--sticky");
+  }
+})
