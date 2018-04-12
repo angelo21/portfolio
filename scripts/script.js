ede710796f1efd7c1d5a87d9b64d83be3006c164
Tom LanFranca <ittechtommy@gmail.com>
2018-04-12 15:18:56 -0500

    Added functionality to clear form upon submission

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //FUNCTIONALITY FOR INFO BOX DISAPPERING ON SCROLL
 
 let header = document.querySelector(".header__info");
@@ -28,35 +27,18 @@ window.addEventListener("scroll", () => {
   } else {
     navBar.classList.remove("header__nav--sticky");
   }
-=======
-//FUNCTIONALITY FOR INFO BOX DISAPPERING ON SCROLL
-
-let header = document.querySelector(".header__info");
-let scrollDown = 0;
-
-window.addEventListener("scroll", () => {
-  let currentScroll = window.pageYOffset;
-
-  if (currentScroll > scrollDown) {
-    header.classList.add("header__info--hidden");
-  } else {
-    header.classList.remove("header__info--hidden");
-  }
-  
-  scrollDown = window.pageYOffset;
-});
-
-//FUNCTIONALITY FOR STICKY NAVIGATION BAR
-
-
-
-window.addEventListener("scroll", () => {
-  let navBar = document.querySelector(".header__nav");
-
-  if(window.pageYOffset >= 650) {
-    navBar.classList.add("header__nav--sticky");
-  } else {
-    navBar.classList.remove("header__nav--sticky");
-  }
->>>>>>> db12f2b573c5cbce502f20346628466e3d40b68b
 })
+
+//RESET FORM ON SUBMISSION
+
+function resetForm() {
+  let form = document.querySelector(".js-form");
+
+  form.addEventListener("submit", () => {
+    setTimeout(() => {
+      form.reset();
+    },1000);
+  });
+}
+
+resetForm();
